a84c1eb7f8a209df84e37ea119c47519af3790c0
Jakub Mroz <115979017+jakmro@users.noreply.github.com>
2024-11-08 18:30:13 +0100

    Add Logo to docs (#7)

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -3,6 +3,8 @@ import styles from './styles.module.css';
 import Layout from '@theme/Layout';
 import HomepageStartScreen from '@site/src/components/Hero/StartScreen';
 import { HireUsSection } from '@swmansion/t-rex-ui';
+import LogoHero from '@site/static/img/logo-hero.svg';
+import HomepageButton from '@site/src/components/HomepageButton';
 
 function Home() {
   return (
@@ -11,6 +13,26 @@ function Home() {
       description="Declarative way to run AI models in React Native on device, powered by ExecuTorch.">
       <div className={styles.container}>
         <HomepageStartScreen />
+      </div>
+      <div
+        style={{
+          marginTop: '300px',
+          marginBottom: '350px',
+          gap: '30px',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <LogoHero />
+        <h2>This site is under development</h2>
+        <a href="/react-native-executorch/docs/fundamentals/getting-started">
+          <HomepageButton
+            href="/react-native-executorch/docs/fundamentals/getting-started"
+            title="Checkout the docs"
+          />
+        </a>
       </div>
       <div className={styles.container}>
         <HireUsSection
