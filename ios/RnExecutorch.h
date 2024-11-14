1fddd50a8b6fe699f316a6163d7a5fa0be7fe861
Norbert Klockiewicz <Nklockiewicz12@gmail.com>
2024-11-14 12:50:54 +0100

    fix: remove import which crashes ios during build (#16)

--- ios/RnExecutorch.h
+++ ios/RnExecutorch.h
@@ -1,13 +1,6 @@
-
-#ifdef RCT_NEW_ARCH_ENABLED
-#import "RNRnExecutorchSpec.h"
-
-@interface RnExecutorch : NSObject <NativeRnExecutorchSpec>
-#else
 #import <React/RCTBridgeModule.h>
 #import <React/RCTEventEmitter.h>
 
 @interface RnExecutorch : RCTEventEmitter <RCTBridgeModule>
-#endif
 
 @end
